61b80b580b6f47ec06ea3a78236aaf93b8345314
Oscar El <opelm1@gmail.com>
2024-06-20 23:32:20 +1000

    Update

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,21 +1,11 @@
-﻿string[] pallets = { "B14", "A11", "B12", "A13" };
-Console.WriteLine("");
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
 
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
+string[] wordArr = pangram.Split(" ");
+
+foreach (string s in wordArr)
 {
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-pallets[4] = "C01";
-pallets[5] = "C02";
-
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
+    char[] sArr = s.ToCharArray();
+    Array.Reverse(sArr);
+    string result = new String(sArr);
+    Console.Write(result + " ");
 }
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf8719b35ce2ebe47438beca139b52193676201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ec1ad666f99c47fb2543ca7370dc8ad70f99ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
